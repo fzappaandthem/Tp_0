1536ba82cab9c2aef3dfe374f714c75afe94bee3
Gastón <fzappaandthem@gmail.com>
2020-08-31 19:49:37 -0300

    Log dinamico

---
+++ tp0.c
@@ -0,0 +1,96 @@
+/*
+ * main.c
+ *
+ *  Created on: 28 feb. 2019
+ *      Author: utnso
+ */
+
+#include "tp0.h"
+
+
+
+int main(void)
+{
+	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
+	int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+
+	t_log* logger;
+	t_config* config;
+
+	logger = iniciar_logger();
+
+	//Loggear "soy un log"
+	log_info(logger, "soy un log");
+	
+	leer_consola(logger);
+
+	config = leer_config();
+
+	//asignar valor de config a la variable valor
+	valor = config_get_string_value(config, "CLAVE");
+	
+	//Loggear valor de config
+	log_info(logger, valor);
+
+
+
+	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+
+	//antes de continuar, tenemos que asegurarnos que el servidor esté corriendo porque lo necesitaremos para lo que sigue.
+
+	//crear conexion
+
+	//enviar CLAVE al servirdor
+
+	paquete(conexion);
+
+	
+	config_destroy(config);
+	log_destroy(logger);
+	terminar_programa(conexion, logger, valor);
+}
+
+t_log* iniciar_logger(void)
+{
+	log_create("tp0.log", "tp0",true, LOG_LEVEL_INFO);
+}
+
+t_config* leer_config(void)
+{
+	config_create("tp0.config");
+}
+
+void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	//El primero te lo dejo de yapa
+
+	while (strlen (leido = readline(">")) > 0)
+	{
+		log_info(logger, leido);
+		free(leido);
+	}
+	
+
+	free(leido);
+
+}
+
+void paquete(int conexion)
+{
+	//Ahora toca lo divertido!
+
+	char* leido;
+	t_paquete* paquete;
+
+
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	//Y por ultimo, para cerrar, hay que liberar lo que utilizamos (conexion, log y config) con las funciones de las commons y del TP mencionadas en el enunciado
+}
